8a221df54d3b48d375e7b374765a82257cd6ee75
LBDCode <libby.duggan@gmail.com>
2019-09-02 14:04:00 -0400

    folder structure

---
+++ app/webpack.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+
+  entry: './frontendapp/src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/frontendapp/static/frontend'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+
+};
